eaa53603cb1402231bbc74b591b2e3505618007d
Peter Godspower <iamgpower3@gmail.com>
2021-06-23 02:34:05 +0100

    added loading spinner for better UX

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-document.getElementById('loan-form').addEventListener('submit', generateResult);
+const spinner = document.getElementById('loading');
+const resultsCard = document.getElementById('results');
 
-function generateResult(e) {
-	// console.log('calculating...');
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+	resultsCard.style.display = 'none';
+	spinner.style.display = 'block';
+	// clearError();
+	setTimeout(generateResult, 2000);
+	e.preventDefault();
+});
+
+function generateResult() {
 	// capturing UI elements
 	const amount = document.getElementById('amount');
 	const interest = document.getElementById('interest');
@@ -11,5 +19,61 @@ function generateResult(e) {
 	const totalInterest = document.getElementById('total-interest');
 
 	const principal = parseFloat(amount.value);
-	e.preventDefault();
+	const generatedInterest = parseFloat(interest.value) / 100 / 12;
+	const generatedPayments = parseFloat(years.value) * 12;
+
+	// Compute monthly payment
+	const x = Math.pow(1 + generatedInterest, generatedPayments);
+	const monthly = (principal * x * generatedInterest) / (x - 1);
+
+	if (isFinite(monthly)) {
+		monthlyPayment.value = monthly.toFixed(2);
+		totalPayment.value = (monthly * generatedPayments).toFixed(2);
+		totalInterest.value = (monthly * generatedPayments - principal).toFixed(2);
+		const showInfo = document.createElement('div');
+		showInfo.className = 'alert alert-info';
+
+		showInfo.appendChild(
+			document.createTextNode(
+				`Loan Amount of N${amount.value} with interest of ${interest.value}% at a period of ${years.value} years`,
+			),
+		);
+
+		// Get elements
+		const resultsDiv = document.querySelector('#results');
+		const resultsHeading = document.querySelector('.title');
+		resultsDiv.insertBefore(showInfo, resultsHeading);
+
+		amount.value = '';
+		interest.value = '';
+		years.value = '';
+
+		resultsCard.style.display = 'block';
+		spinner.style.display = 'none';
+	} else {
+		displayError('Please check the input figures');
+		resultsCard.style.display = 'none';
+		spinner.style.display = 'none';
+		// console.log('Please check the input figures');
+	}
+	// console.log(generateInterest);
+}
+
+function displayError(message) {
+	const errorDisplay = document.createElement('div');
+	errorDisplay.className = 'alert alert-danger';
+
+	errorDisplay.appendChild(document.createTextNode(message));
+
+	// Get elements
+	const card = document.querySelector('.card');
+	const heading = document.querySelector('.heading');
+	card.insertBefore(errorDisplay, heading);
+
+	// clear error after 4sec
+	setTimeout(clearError, 2000);
+
+	function clearError() {
+		errorDisplay.remove();
+	}
 }
